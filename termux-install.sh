2277d852c0116202c42f73490fef1a75bdd2298d
SAYDOG <55991841+saydog@users.noreply.github.com>
2020-05-13 09:04:52 +0700

    termux installations

--- termux-install.sh
+++ termux-install.sh
@@ -1,2 +1,2 @@
-!/usr/bin/bash
+#!/usr/bin/bash
 pkg install -y figlet && git clone https://github.com/saydog/figlet-fonts && cd figlet-fonts/fonts && cp * ~/../usr/share/figlet;mv ~/figlet-fonts/figfonts ~/../usr/bin && chmod 700 ~/../usr/bin/figfonts;cd $HOME && rm -rf ~/figlet-fonts
